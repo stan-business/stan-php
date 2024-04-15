9055f87f71f04dec3cc4d054cd159cb59d5c2c23
Slals <jonathan@blocs.fr>
2024-04-15 22:02:55 +0200

    Adds checkoutApi

--- lib/Api/StanClient.php
+++ lib/Api/StanClient.php
@@ -99,6 +99,7 @@ class StanClient
         $this->connectApi = new ConnectApi($this);
         $this->customerApi = new CustomerApi($this);
         $this->paymentApi = new PaymentApi($this);
+        $this->checkoutApi = new CheckoutApi($this);
         $this->refundApi = new RefundApi($this);
         $this->userApi = new UserApi($this);
 
